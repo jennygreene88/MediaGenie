f30f7e83d5ae5609cf124a8adac7a4295300568c
Ryan Plant <ryan@phunkatronixxx.net>
2014-11-16 12:41:19 +1100

    Correct for style. No functionality change.

--- app.rb
+++ app.rb
@@ -1,50 +1,50 @@
 before do
-  @db = SQLite3::Database.new("discs.db")
+  @db = SQLite3::Database.new('discs.db')
 end
 
 
 get '/' do
-  @users = @db.execute("SELECT name FROM owners ORDER BY name ASC")
+  @users = @db.execute('SELECT name FROM owners ORDER BY name ASC')
   erb :main
 end
 
 
 get '/queue' do
-  @unissued_movies = @db.execute("SELECT owners.name, movies.title FROM movies JOIN owners ON owners.id = movies.owner WHERE issued = 0")
+  @unissued_movies = @db.execute('SELECT owners.name, movies.title FROM movies JOIN owners ON owners.id = movies.owner WHERE issued = 0')
   erb :queue
 end
 
 
 get '/stats' do
-  @issued           = @db.execute("SELECT (SELECT COUNT(*) FROM MOVIES) as count1, (SELECT COUNT(*) FROM tv) as count2")[0].reduce(:+)
-  @heaviest_movies  = @db.execute("SELECT name from owners where id = (SELECT owner from movies group by owner order by owner asc limit 1)")[0][0]
-  @heaviest_tv      = @db.execute("SELECT name from owners where id = (SELECT owner from tv group by owner order by owner asc limit 1)")[0][0]
-  @subscriptions    = @db.execute("SELECT count(*) FROM subscriptions")[0][0]
-  @most_discs       = @db.execute("select show from (select show, count(*) from tv group by show limit 1)")[0][0]
-  @most_popular     = @db.execute("select title from (select title, count(*) as count1 from subscriptions group by title order by count1 desc limit 1)")[0][0]
-  @users            = @db.execute("select count(*) from owners")[0][0]
-  @discs_per_day    = sprintf "%.05f", @issued/((Time.now.to_i-Time.new("2014-01-01").to_i)/60/60/24).to_f
+  @issued           = @db.execute('SELECT (SELECT COUNT(*) FROM MOVIES) as count1, (SELECT COUNT(*) FROM tv) as count2')[0].reduce(:+)
+  @heaviest_movies  = @db.execute('SELECT name from owners where id = (SELECT owner FROM movies GROUP by owner ORDER BY owner ASC LIMIT 1)')[0][0]
+  @heaviest_tv      = @db.execute('SELECT name from owners where id = (SELECT owner FROM tv GROUP BY owner ORDER BY owner ASC LIMIT 1)')[0][0]
+  @subscriptions    = @db.execute('SELECT count(*) FROM subscriptions')[0][0]
+  @most_discs       = @db.execute('SELECT show FROM (select show, count(*) FROM tv GROUP BY show LIMIT 1)')[0][0]
+  @most_popular     = @db.execute('SELECT title FROM (select title, count(*) AS count1 FROM subscriptions GROUP BY title ORDER BY count1 DESC LIMIT 1)')[0][0]
+  @users            = @db.execute('SELECT count(*) FROM owners')[0][0]
+  @discs_per_day    = sprintf '%.05f', @issued/((Time.now.to_i-Time.new('2014-01-01').to_i)/60/60/24).to_f
 
   erb :stats
 end
 
 
 get '/:name' do
-  @allshows = @db.prepare("select distinct show from tv where owner = (SELECT id FROM owners WHERE name = ?)").execute(params[:name])
+  @allshows = @db.prepare('SELECT DISTINCT show FROM tv WHERE owner = (SELECT id FROM owners WHERE name = ?)').execute(params[:name])
   
   # Build a multidimensional array like [[Seinfeld, SQL result set for all Seinfeld discs], [Hannibal, SQL result set for all Hannibal discs]]
   @watched_shows = []
-  @allshows.each {|sub| @watched_shows << [sub[0], @db.prepare("SELECT episodes FROM tv WHERE show = ? AND owner = (SELECT id FROM owners WHERE name = ?)").execute(sub[0], params[:name])]}
+  @allshows.each {|sub| @watched_shows << [sub[0], @db.prepare('SELECT episodes FROM tv WHERE show = ? AND owner = (SELECT id FROM owners WHERE name = ?)').execute(sub[0], params[:name])]}
 
   # Build a string of watched shows to use as a header on the resulting page.
   @watch_string_array = []
-  @db.prepare("select title from subscriptions where name = ?").execute(params[:name]).each { |sub| @watch_string_array << %Q*<a href="/shows/#{URI::encode(sub[0])}">#{sub[0]}</a>* }
+  @db.prepare('SELECT TITLE FROM subscriptions WHERE name = ?').execute(params[:name]).each { |sub| @watch_string_array << %Q*<a href="/shows/#{URI::encode(sub[0])}">#{sub[0]}</a>* }
   (@watch_string_array.length == 0) ?
-      @watch_string_array = "nothing" :
-      @watch_string_array = @watch_string_array.join(", ").gsub(/, (?!.*, )/, " and ")
+      @watch_string_array = 'nothing' :
+      @watch_string_array = @watch_string_array.join(", ").gsub(/, (?!.*, )/, ' and ')
 
   # Get the list of all movies burnt for this person.
-  @movies = @db.prepare("select title from movies where owner = (SELECT id FROM owners WHERE name = ?)").execute(params[:name])
+  @movies = @db.prepare('SELECT TITLE FROM movies WHERE owner = (SELECT id FROM owners WHERE name = ?)').execute(params[:name])
 
   erb :subs
 end
@@ -52,9 +52,9 @@ end
 
 get '/shows/:show' do
   @viewers = []
-  @db.prepare("SELECT name FROM subscriptions WHERE title = ? ORDER BY name ASC").execute(params[:show]).each {|viewer| @viewers << viewer[0]}
+  @db.prepare('SELECT name FROM subscriptions WHERE title = ? ORDER BY name ASC').execute(params[:show]).each {|viewer| @viewers << viewer[0]}
   @viewers = @viewers.map { |name| "<a href='../../#{name}'>#{name}</a>" }
-  @viewers = @viewers.join(", ").gsub(/, (?!.*, )/, " and ") # the gsub is to replace the final comma in the list with ", and "
+  @viewers = @viewers.join(", ").gsub(/, (?!.*, )/, ' and ') # the gsub is to replace the final comma in the list with ", and "
 
   erb :viewers
 end
